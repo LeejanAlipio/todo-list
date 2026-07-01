6167a6b36dc5312f4ce447e4f093dcf6a81b5b91
LeejanAlipio <alipioml0131@gmail.com>
2026-07-01 15:05:46 +0800

    feat: Add project class

---
+++ src/modules/project.js
@@ -0,0 +1,21 @@
+import Todo from "./todo.js";
+
+export default class Project {
+  constructor(name, description = '') {
+    this.name = name;
+    this.description = description;
+    this.projectTasks = [];
+  }
+
+  addTodo(name, description, dueDate, priority, status) {
+    const newTodo = new Todo(name, description, dueDate, priority, status)
+    this.projectTasks.push(newTodo);
+  }
+
+  removeTodo(id) {
+    const todoIndex = this.projectTasks.findIndex(todo => todo.id == id);
+    if (todoIndex !== -1) {
+      this.projectTasks.splice(todoIndex, 1);
+    }
+  }
+}
