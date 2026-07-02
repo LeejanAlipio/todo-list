c3515c7188006b46c305122ed4c55991a3a91a57
LeejanAlipio <alipioml0131@gmail.com>
2026-07-02 15:19:14 +0800

    feat: Add localStorage persistence

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,8 +1,52 @@
 import Todo from "./todo.js";
 
+const STORAGE_KEY = 'todo-list-projects';
 const projectList = [];
 
 export const getProjectList = () => [...projectList];
+
+export const saveProjects = () => {
+  const data = projectList.map(project => ({
+    name: project.name,
+    description: project.description,
+    projectTasks: project.projectTasks.map(task => ({
+      id: task.id,
+      title: task.title,
+      description: task.description,
+      dueDate: task.dueDate,
+      priority: task.priority,
+      status: task.status,
+    })),
+  }));
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+};
+
+export const loadProjects = () => {
+  const raw = localStorage.getItem(STORAGE_KEY);
+  if (!raw) return false;
+
+  try {
+    const data = JSON.parse(raw);
+    data.forEach(projectData => {
+      const project = new Project(projectData.name, projectData.description);
+      projectData.projectTasks?.forEach(taskData => {
+        project.projectTasks.push(new Todo(
+          taskData.title,
+          taskData.description,
+          taskData.dueDate,
+          taskData.priority,
+          taskData.status,
+          taskData.id,
+        ));
+      });
+      projectList.push(project);
+    });
+    return true;
+  } catch {
+    return false;
+  }
+};
 
 export const createDefaultProject = () => {
   if (projectList.length === 0) {
@@ -29,7 +73,7 @@ export default class Project {
   }
 
   removeTodo(id) {
-    const todoIndex = this.projectTasks.findIndex(todo => todo.id == id);
+    const todoIndex = this.projectTasks.findIndex(todo => todo.id === id);
     if (todoIndex !== -1) {
       this.projectTasks.splice(todoIndex, 1);
     }
